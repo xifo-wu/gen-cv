9c37359812f2cbfecd42d0c44d5069c6f99cc0c0
Xifo <xifo.wu@gmail.com>
2023-01-03 10:50:44 +0800

    fix: 修复简历 Paper 高度样式错误

--- lib/components/Resume/commonStyles.ts
+++ lib/components/Resume/commonStyles.ts
@@ -2,7 +2,7 @@ export default {
   resumePaper: {
     margin: 'auto',
     width: '210mm',
-    height: '297mm',
+    height: '100%',
     minWidth: '210mm',
     minHeight: '297mm',
     background: '#fff',
